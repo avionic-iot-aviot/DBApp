43bfb897bb590227dcb05b931804d878ec6831de
Marco Cavalli <cmarcos46@gmail.com>
2021-05-04 18:10:42 +0200

    removed the watch prop which would restart the app

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,12 +1,8 @@
-
-
-
 module.exports = {
     apps: [
         {
             name: "dbapp",
             script: "./dist/main.js",
-            watch: true,
             instance_var: 'INSTANCE_ID',
             env: {
                 "NODE_ENV": "staging"
