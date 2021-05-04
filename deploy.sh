b9a410814ca8335e774ae6d62e87fdda01548e8f
Marco Cavalli <cmarcos46@gmail.com>
2021-05-04 11:26:03 +0200

    Removed the creation of ecosystem file in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,24 +14,8 @@ pm2 delete 0
 cd ~/$PROJECT_FOLDER/backend
 npm install
 npm run be:build
+NODE_ENV=staging knex migrate:up
+pm2 start ecosystem.config.js && cd ~/ && pm2 startup openrc > pm2_startup_output && tail -n 1 pm2_startup_output > pm2_startup.sh && chmod a+rwx pm2_startup.sh && ./pm2_startup.sh && pm2 save
 cd ~/$PROJECT_FOLDER/backend
-cat << EOF > ecosystem.config.js
-module.exports = {
-  apps : [
-      {
-        name: "dbapp",
-        script: "./dist/main.js",
-        watch: true,
-        instance_var: 'INSTANCE_ID',
-        env: {
-            "NODE_ENV": "staging"
-        }
-      }
-  ]
-}
-EOF
-pm2 start ecosystem.config.js && cd ~/ && pm2 startup openrc > pm2_startup_output &&
-tail -n 1 pm2_startup_output > pm2_startup.sh && chmod a+rwx pm2_startup.sh && ./pm2_startup.sh && pm2 save
-pm2 stop 0
-pm2 start 0 & pm2 logs 0
+pm2 logs 0
 
