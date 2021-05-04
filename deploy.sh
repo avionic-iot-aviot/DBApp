c70bc0e9b14a457e9b53602157e4a852e273b1f6
Marco Cavalli <cmarcos46@gmail.com>
2021-05-04 11:47:31 +0200

    calling the new npm script for latest migrations

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ pm2 delete 0
 cd ~/$PROJECT_FOLDER/backend
 npm install
 npm run be:build
-NODE_ENV=staging knex migrate:up
+npm run be:migrate:staging
 pm2 start ecosystem.config.js && cd ~/ && pm2 startup openrc > pm2_startup_output && tail -n 1 pm2_startup_output > pm2_startup.sh && chmod a+rwx pm2_startup.sh && ./pm2_startup.sh && pm2 save
 cd ~/$PROJECT_FOLDER/backend
 pm2 logs 0
