5417ef0868d8180053388ff09d08f208830457de
Marco Cavalli <cmarcos46@gmail.com>
2021-05-04 12:24:33 +0200

    Removed the create query and few commands unneeded on pod startup (such as the pm2 delete 0)

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,4 @@
 mkdir /db
-sqlite3 /db/mydb.sqlite < create.sql
-
-
 PROJECT_FOLDER="DBApp"
 
 echo 'Scarico da Github'
@@ -10,12 +7,10 @@ git clone https://github.com/avionic-iot-aviot/DBApp
 
 echo 'Avvio di deploy'
 
-pm2 delete 0
+#pm2 delete 0
 cd ~/$PROJECT_FOLDER/backend
-npm install
-npm run be:build
-npm run be:migrate:staging
-pm2 start ecosystem.config.js && cd ~/ && pm2 startup openrc > pm2_startup_output && tail -n 1 pm2_startup_output > pm2_startup.sh && chmod a+rwx pm2_startup.sh && ./pm2_startup.sh && pm2 save
-cd ~/$PROJECT_FOLDER/backend
-pm2 logs 0
+npm install && npm run be:build && npm run be:migrate:staging && pm2 start ecosystem.config.js && pm2 logs
+#pm2 start ecosystem.config.js && cd ~/ && pm2 startup openrc > pm2_startup_output && tail -n 1 pm2_startup_output > pm2_startup.sh && chmod a+rwx pm2_startup.sh && ./pm2_startup.sh && pm2 save
+#cd ~/$PROJECT_FOLDER/backend
+#pm2 logs 0
 
