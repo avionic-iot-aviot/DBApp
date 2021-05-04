e703515fe8c39d8f4a25c3865d01931bedadf44f
Marco Cavalli <cmarcos46@gmail.com>
2021-05-04 12:46:50 +0200

    rowdata in frontend routes have a different structure. Now DBApp converts them to allow the frontend to retrieve them

--- backend/src/routes/frontendRoutes.ts
+++ backend/src/routes/frontendRoutes.ts
@@ -17,17 +17,34 @@ var cors = require('cors');
 router.use(cors());
 router.options('*', cors());
 
+function mapForFrontend(data: any[]) {
+    return _.map(data, (el: any) => {
+        return {
+            Device_id: el.device_id,
+            Mac: el.mac_address,
+            Default_Name: el.default_name,
+            Current_Name: el.current_name,
+            Created_at: el.created_at,
+            Updated_at: el.updated_at
+        }
+    }
+    );
+}
+
 router.get('/getAllDevices', async (req, res) => {
     //const body = req.body;
-    var ip = req.connection.remoteAddress.split(":")[((req.connection.remoteAddress.split(":")).length)-1]
+    var ip = req.connection.remoteAddress.split(":")[((req.connection.remoteAddress.split(":")).length) - 1]
     try {
         //const params = body && body.params ? body.params : null;
-        console.log("frontendRoutes received("+ip+") ");
-        Utilities.log("frontendRoutes received("+ip+") ");
-        
-        //frontendService.GetAllElements();
+        console.log("frontendRoutes received(" + ip + ") ");
+        Utilities.log("frontendRoutes received(" + ip + ") ");
+
+        // frontendService.GetAllElements();
         let rowdata = await deviceStore.getAllElements();
-        res.status(HttpStatus.OK).send(rowdata);
+        // res.status(HttpStatus.OK).send(rowdata);
+        const formattedData = mapForFrontend(rowdata);
+        //console.log("formattedData: ", formattedData);
+        res.status(HttpStatus.OK).send(formattedData);
     } catch (error) {
         res.status(HttpStatus.OK).send(error);
     }
@@ -35,11 +52,11 @@ router.get('/getAllDevices', async (req, res) => {
 
 router.post('/configureDevice', async (req, res) => {
     const body = req.body;
-    var ip = req.connection.remoteAddress.split(":")[((req.connection.remoteAddress.split(":")).length)-1]
+    var ip = req.connection.remoteAddress.split(":")[((req.connection.remoteAddress.split(":")).length) - 1]
     try {
         const params = body && body.params ? body.params : null;
-        console.log("frontendRoutes received("+ip+"): ","PARAMS", params);
-        Utilities.log("frontendRoutes received("+ip+"): " + "PARAMS " + params);
+        console.log("frontendRoutes received(" + ip + "): ", "PARAMS", params);
+        Utilities.log("frontendRoutes received(" + ip + "): " + "PARAMS " + params);
         const result = await delay(1000);
         frontendService.NewElements(params.device);
         res.status(HttpStatus.OK).send();
