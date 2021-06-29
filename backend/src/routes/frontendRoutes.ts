2f1d95a6032a9c268b35cefd8d9f34ac4c4e6b5f
Marco Cavalli <cmarcos46@gmail.com>
2021-06-29 16:12:21 +0200

    Update frontendRoutes.ts

--- backend/src/routes/frontendRoutes.ts
+++ backend/src/routes/frontendRoutes.ts
@@ -31,7 +31,7 @@ function mapForFrontend(data: any[]) {
 }
 
 router.get('/getAllDevices', async (req, res) => {
-    const show_not_active = req.query && req.query.show_not_active === true;
+    const show_not_active = req.query && (req.query.show_not_active as boolean) === true;
     var ip = req.connection.remoteAddress.split(":")[((req.connection.remoteAddress.split(":")).length) - 1]
     try {
         //const params = body && body.params ? body.params : null;
